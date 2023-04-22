6829249107a37196338b6c785b2d1467577c90b4
mul <1842636270@qq.com>
2023-04-22 18:54:14 +0800

    tabbar

---
+++ pages/schedule/schedule.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		schedule
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<!-- <style lang="scss">
+
+</style>
+ -->
